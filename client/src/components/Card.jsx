e47a9cc6c0d84d8427d1f10a29bc2ae8e6914839
George <13143101750@163.com>
2023-06-08 22:19:37 +1000

    feat: scrollbar and create form style

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -11,7 +11,7 @@ const Card = ({ _id, name, prompt, photo }) => {
         src={photo}
         alt={prompt}
       />
-      <div className="group-hover:flex flex-col max-h-[94.5%] hidden absolute bottom-0 left-0 right-0 bg-[#10131f] m-2 p-4 rounded-md">
+      <div className="group-hover:flex flex-col max-h-[94.5%] hidden absolute bottom-0 left-0 right-0 bg-[#10131f] opacity-50 m-2 p-4 rounded-md">
         <p className="text-white text-md overflow-y-auto prompt">{prompt}</p>
         <div className="mt-5 flex justify-between items-center gap-2">
           <div className="flex items-center gap-2">
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -83,10 +83,10 @@ const CreatePost = () => {
   };
 
   return (
-    <section className="max-w-7xl mx-auto">
+    <section className="flex flex-col max-w-7xl mx-auto items-center">
       <div>
         <h1 className="font-extrabold text-[#6469ff] text-[32px]">Create</h1>
-        <p className="mt-2 text-[#666e75] text-[16px] max-w-[500px]">
+        <p className="mt-2 text-[#666e75] text-[16px] max-w-7xl">
           Create imaginative and visually stunning images through DALL-E AI and
           share them with the community
         </p>
@@ -113,7 +113,7 @@ const CreatePost = () => {
             handleSurpriseMe={handleSurpriseMe}
           />
 
-          <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
+          <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-auto p-3 h-auto flex justify-center items-center">
             {form.photo ? (
               <img
                 src={form.photo}
@@ -140,7 +140,7 @@ const CreatePost = () => {
           <button
             type="button"
             onClick={generateImage}
-            className="text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text center"
+            className="text-white bg-green-700 font-medium rounded-md text-sm w-full px-5 py-2.5 text center"
           >
             {generatingImg ? "Generating..." : "Generate"}
           </button>
@@ -153,7 +153,7 @@ const CreatePost = () => {
           </p>
           <button
             type="submit"
-            className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+            className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full px-5 py-2.5 text-center"
           >
             {loading ? "Sharing..." : "Share with the community"}
           </button>
