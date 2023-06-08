354630b561bf4735e1af9dc65c83e4277d256481
George <13143101750@163.com>
2023-06-08 16:03:34 +1000

    feat: tab icon

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -79,7 +79,7 @@ const CreatePost = () => {
   return (
     <section className="max-w-7xl mx-auto">
       <div>
-        <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
+        <h1 className="font-extrabold text-[#6469ff] text-[32px]">Create</h1>
         <p className="mt-2 text-[#666e75] text-[16px] max-w-[500px]">
           Create imaginative and visually stunning images through DALL-E AI and
           share them with the community
